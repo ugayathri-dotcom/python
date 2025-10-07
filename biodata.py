fbce915fc625554b770894ac063211224484eade
ugayathri <ugayathriaarahdhya@gmail.com>
2025-10-07 21:46:43 +0530

    first commit

---
+++ biodata.py
@@ -0,0 +1,19 @@
+name= "Name: U.Gayathri"
+dob= "DOB : 22/4/1996"
+phn= "Phone No: 7339004950"
+mail="gayathrianuuthaya@gmail.com"
+br="--------------------------------------------------------------------------------"
+ca="CAREEER SUMMARY :CSE graduate with 2+ years of professional experience in administration and coordination.\nStrong foundation in programming (C, C++, Java, Python, HTML, CSS) and problem-solving.Currently upskilling in Python and web technologies.\nSeeking opportunities in IT support, Data Entry, or Administrative roles where I can contribute technical knowledge, organizational skills, and adaptability"
+t="Technical skills : C,C++,JAVA,PYTHON"
+a="Leadership & Strengths \n Student Placement Coordinator – CSE Department.\n Event Coordinator – Department Symposium (2016)"
+
+
+print(name)
+print( dob)
+print(phn)
+print("GMAIL:" +mail)
+print(br)
+print(ca)
+print(t)
+print(a)
+
