198f4f793c131313bc5f648a95ce469707191203
ugayathri <ugayathriaarahdhya@gmail.com>
2025-10-08 20:43:21 +0530

    day2

---
+++ t5bank.py
@@ -0,0 +1,9 @@
+name=input("Enter your Name:")
+r=input("Enter your mail:")
+t=int(input("Enter Mobile No:"))
+e=input("Enter Bank Name:")
+m=input("Enter IFSC Code:")
+s=int(input("Available Balance"))
+print(f"Name:{name}\nMail:{r} \nMobile No:{t} \nBank Name:{e} \nIFSC Code:{m} \nAvailable Balance:{s}")
+
+
