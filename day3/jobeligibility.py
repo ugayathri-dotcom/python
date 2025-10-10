f68f4d0ecdce0eadaf052661b3916caec5337d22
ugayathri <ugayathriaarahdhya@gmail.com>
2025-10-10 11:06:46 +0530

    updated

--- day3/jobeligibility.py
+++ day3/jobeligibility.py
@@ -7,8 +7,22 @@ if a >= 85:
             d=int(input("Enter Marks in HR"))
             if d >= 95:
                 print("Candidate Selected")
+                t=a+b+c+d
+                if t >390 and t <= 400:
+                    print("Salary Eligibility is Rs.30000")
+                elif t > 380 and t <=390:
+                    print("Salary Eligibility is Rs.28000")
+                elif t > 370 and t <= 380:
+                    print("Salary Eligibility is Rs.25000")
+                elif t > 350 and t <= 370:
+                    print("Salary Eligibility is Rs.30000")
+                    
+            
+                
             else: print("Disqualified in HR round")
         else: print("Disqualified in Technical round")
     else: print("Disqualified in GD round")
 else: print("Disqualified in Aptitude round")
+
+
     
